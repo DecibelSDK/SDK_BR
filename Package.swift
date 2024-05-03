10fe3fe172f599a4aa08d8449a769b0aae251adf
Victor Iglesias <viglesiasalcon@medallia.com>
2024-05-03 13:33:01 +0200

    Folder prepared.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//  
+//  Copyright © 2023 Medallia. Use subject to licensing terms.
+
+import PackageDescription
+
+let package = Package(
+    name: "sdk-br",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "sdk-br",
+            targets: ["SDK_BR"]),
+    ],
+    targets: [
+            .binaryTarget(
+                name: "SDK_BR",
+                path: "SDK-BR.xcframework"
+            )
+    ]
+)
